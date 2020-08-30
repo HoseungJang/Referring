bcaeb9566366a3b026ebb81b0023439fdda63640
HoseungJang <hsjang.dev@gmail.com>
2020-08-30 17:38:11 +0900

    remove TitleBar

--- client/src/containers/Home/Desktop/index.tsx
+++ client/src/containers/Home/Desktop/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-import { TitleBar } from "../../../components/Layout/TitleBar"
-
 export const Home: React.FC = () => {
     return (
-        <TitleBar />
+        <div></div>
     )
 };

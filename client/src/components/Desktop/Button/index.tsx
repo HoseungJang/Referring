ff16fe3727f4ee41cbdea8c0db174fe51d48a91f
HoseungJang <hsjang.dev@gmail.com>
2020-08-30 17:40:12 +0900

    add desktop button

---
+++ client/src/components/Desktop/Button/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+import { Link } from "react-router-dom";
+
+export const Button: React.FC<{ path: string }> = (props) => {
+    return (
+        <Container>
+            <Link to={props.path}>
+                <div className="wrap-text">
+                    {props.children}
+                </div>
+            </Link>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    text-align: center;
+
+    a {
+        width: 400px;
+        border: 2px solid lightgrey;
+        color: black;
+        text-decoration: none;
+
+        .wrap-text {
+            padding: 10px;
+        }
+    }
+`;

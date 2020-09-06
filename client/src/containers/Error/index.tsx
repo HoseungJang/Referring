1950983a0f8bd319b1054814899c17034f7a7af0
HoseungJang <hsjang.dev@gmail.com>
2020-09-06 23:59:11 +0900

    delete desktop/mobile separation and apply media query

--- client/src/containers/Error/index.tsx
+++ client/src/containers/Error/index.tsx
@@ -1,19 +1,66 @@
 import React from "react";
+import styled from "styled-components";
 
-import { NotFound as Desktop } from "./Desktop";
-import { NotFound as Mobile } from "./Mobile";
+import { Warning } from "@material-ui/icons";
+import { Typography } from "../../components/Typography";
+import { Button } from "../../components/Button";
 
-import { useMediaQuery } from "../../hooks/useMediaQuery";
+import { Color } from "../../constants/color";
+import { Device } from "../../constants/device";
 
 export const NotFound: React.FC = () => {
   return (
-    <>
-      {
-        {
-          desktop: <Desktop />,
-          mobile: <Mobile />,
-        }[useMediaQuery()]
-      }
-    </>
+    <Container>
+      <div className="wrap">
+        <Warning className="wrap-img" />
+        <div className="wrap-msg">
+          <Typography.ErrorMessage>
+            페이지를 찾을 수 없습니다
+          </Typography.ErrorMessage>
+        </div>
+        <div className="wrap-button">
+          <Button path="/">홈으로 돌아가기</Button>
+        </div>
+      </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  .wrap {
+    width: 100%;
+    height: 100%;
+    color: ${Color.MainColor};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .wrap-img {
+      font-size: 130px;
+    }
+
+    @media ${Device.mobile} {
+      .wrap-img {
+        font-size: 150px;
+      }
+    }
+
+    @media ${Device.desktop} {
+      .wrap-img {
+        font-size: 120px;
+      }
+    }
+
+    .wrap-msg {
+      margin-top: 20px;
+    }
+
+    .wrap-button {
+      margin-top: 10px;
+    }
+  }
+`;

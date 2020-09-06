8a1d6fd0dae51e533a58dae861a675d000e8a5e0
HoseungJang <hsjang.dev@gmail.com>
2020-09-06 22:33:48 +0900

    add min-width for media query

---
+++ client/src/constants/device.ts
@@ -0,0 +1,7 @@
+import { Size } from "./size";
+
+export const Device = {
+  mobile: `(min-width: ${Size.mobile})`,
+  tablet: `(min-width: ${Size.tablet})`,
+  desktop: `(min-width: ${Size.desktop})`,
+};

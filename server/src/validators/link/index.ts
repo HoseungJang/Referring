c019f5753e4af452a7e031797e90c8bccefb0e61
HoseungJang <hsjang.dev@gmail.com>
2020-09-15 21:25:37 +0900

    fix getLinkList request validator

--- server/src/validators/link/index.ts
+++ server/src/validators/link/index.ts
@@ -3,11 +3,15 @@ import { Either, left, right } from "fp-ts/lib/Either";
 
 import { getLinkListRequestScheme } from "./shemes";
 
+import { getLinkListDTO } from "../../services/link/link.dto";
+
 export const getLinkRequestValidator = (
   req: Request
-): Either<Error, Request> => {
-  const { params: path, query, body } = req;
+): Either<Error, getLinkListDTO> => {
+  const { params: path, query, body } = req as any;
   const result = getLinkListRequestScheme.is({ path, query, body });
 
-  return result ? right(req) : left(new Error("invalid request"));
+  return result
+    ? right(query as getLinkListDTO)
+    : left(new Error("invalid request"));
 };

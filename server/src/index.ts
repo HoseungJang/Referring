e1df528a41e795528a522c7b8efc12caf061db99
HoseungJang <hsjang.dev@gmail.com>
2020-09-18 00:00:38 +0900

    add setConnection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
+import "reflect-metadata";
 import { Application } from "express";
 import { pipe } from "fp-ts/pipeable";
 import { fold } from "fp-ts/lib/Either";
 
+import { setConnection } from "./connection";
 import { buildApp } from "./app";
 
 pipe(
-  buildApp(),
+  setConnection(),
+  buildApp,
   fold(
     (err: Error) => console.error(err),
     (app: Application) => app.listen(3000)

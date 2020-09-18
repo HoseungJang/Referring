07a4aca3549f113bf8c0099aad2964934cc8fba3
HoseungJang <hsjang.dev@gmail.com>
2020-09-19 00:18:03 +0900

    add NODE_ENV

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,10 +1,24 @@
+const { NODE_ENV } = process.env;
+
 module.exports = {
-  type: "mysql",
-  host: "reffering-mysql",
-  port: 3306,
-  username: "root",
-  password: "password",
-  database: "reffering",
-  synchronize: true,
-  entities: ["src/entities/**/*.ts"],
-}
+  production: {
+    type: "mysql",
+    host: "reffering-mysql",
+    port: 3306,
+    username: "root",
+    password: "password",
+    database: "reffering",
+    synchronize: true,
+    entities: ["src/entities/**/*.ts"],
+  },
+  development: {
+    type: "mysql",
+    host: "0.0.0.0",
+    port: 3306,
+    username: "root",
+    password: "password",
+    database: "reffering",
+    synchronize: true,
+    entities: ["src/entities/**/*.ts"],
+  }
+}[NODE_ENV]

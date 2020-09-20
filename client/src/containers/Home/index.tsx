8839686b2b9a3a4300f54c6528c84f77ff23bbea
HoseungJang <hsjang.dev@gmail.com>
2020-09-20 22:17:09 +0900

    add list test

--- client/src/containers/Home/index.tsx
+++ client/src/containers/Home/index.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 
-export const Home: React.FC = () => {
+import { Spinner } from "../../components/Spinner";
+
+import { useApiQuery } from "../../hooks/useApi";
+
+import { withPlaceholder } from "../../helpers/withPlaceholder";
+
+const HomeLoaded: React.FC = () => {
+  const list = useApiQuery(
+    "getLinkList",
+    { page: 0, limit: 10 },
+    { suspense: true }
+  );
+  console.log(list.data);
+
   return <div>home</div>;
 };
+
+const HomePlaceholder: React.FC = () => {
+  return <Spinner />;
+};
+
+export const Home = withPlaceholder(HomeLoaded, HomePlaceholder);

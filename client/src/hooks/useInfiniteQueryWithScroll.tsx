7bf5f6d0b7b38943de6b07aa48eab6888aae68d6
HoseungJang <hsjang.dev@gmail.com>
2020-09-23 02:54:12 +0900

    add hook for scrolling with intersection observer

---
+++ client/src/hooks/useInfiniteQueryWithScroll.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+import { PaginatedQueryOperationId, useApiPaginatedQuery } from "./useApi";
+
+export const useInfiniteQueryWithScroll = (
+  operationId: PaginatedQueryOperationId,
+  count: number
+) => {
+  const query = useApiPaginatedQuery(operationId, { limit: count });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && !query.isFetchingMore && query.canFetchMore) {
+      console.log("adsfs");
+      query.fetchMore();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView]);
+
+  return {
+    data: query.data,
+    isFetchingFirst: query.isFetching && !query.isFetchingMore,
+    isFetchingMore: query.isFetching,
+    fetchTriggerElement: <div ref={ref}></div>,
+  };
+};

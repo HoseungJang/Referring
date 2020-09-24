57d961daea3d398a8f11e23a9460f969a5188230
HoseungJang <hsjang.dev@gmail.com>
2020-09-24 23:44:13 +0900

    add button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -6,29 +6,38 @@ import { Link } from "react-router-dom";
 import { Color } from "../../constants/color";
 import { Device } from "../../constants/device";
 
+export const Button: React.FC<{ onClick?: () => void }> = (props) => {
+  return (
+    <Container onClick={props.onClick}>
+      <div className="wrap-content">{props.children}</div>
+    </Container>
+  );
+};
+
 export const LinkButton: React.FC<{ path: string }> = (props) => {
   return (
     <Container>
       <Link to={props.path}>
-        <div className="wrap-text">{props.children}</div>
+        <div className="wrap-content">{props.children}</div>
       </Link>
     </Container>
   );
 };
 
 const Container = styled.div`
+  width: 100%;
   display: flex;
   text-align: center;
 
-  a {
+  > a {
     width: 100%;
 
     border: 2px solid ${Color.ButtonBorder};
     color: ${Color.MainColor};
     text-decoration: none;
+  }
 
-    .wrap-text {
-      padding: 10px;
-    }
+  .wrap-content {
+    padding: 10px;
   }
 `;

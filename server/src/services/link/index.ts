a89cdf376e7354395c01a860f0eab14d887809b3
HoseungJang <hsjang.dev@gmail.com>
2020-09-26 16:47:34 +0900

    add og:image loader

--- server/src/services/link/index.ts
+++ server/src/services/link/index.ts
@@ -1,5 +1,6 @@
 import { TaskEither } from "fp-ts/TaskEither";
 import { DeleteResult } from "typeorm";
+import ogs, { ErrorResult } from "open-graph-scraper";
 
 import { Link } from "../../entities/link";
 
@@ -11,12 +12,26 @@ import {
 } from "../../interfaces/link.dto";
 
 import { makeLinkRepositories } from "../../repositories/link";
+import { pipe } from "fp-ts/lib/pipeable";
+import { tryCatch, fold, left } from "fp-ts/lib/TaskEither";
 
 export const makeLinkService = () => {
   const repository = makeLinkRepositories();
 
   const create = (dto: createDTO): TaskEither<Error, Link> => {
-    return repository.create(dto);
+    return pipe(
+      tryCatch(
+        () => ogs({ url: dto.link }),
+        (err: ErrorResult) => new Error("invalid link")
+      ),
+      fold(
+        (err: Error) => left(err),
+        ({ result }) =>
+          result.success
+            ? repository.create({ img: result.ogImage.url, ...dto })
+            : left(new Error("invalid link"))
+      )
+    );
   };
 
   const getList = (dto: getListDTO): TaskEither<Error, Link[]> => {
